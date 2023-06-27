467a225acb1556fe130f3825f6012827a7bb5c43
iinoten <nteten0313@icloud.com>
2023-06-27 17:23:36 +0900

    Add 修飾詞の追加

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,51 @@
 class HitAndBlow {
-    answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    answer: string[] = [] // 空配列のみだと中身に対しての型推論が働かないので型アノテーションが必要
-    tryCount = 0
+    private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    private answer: string[] = [] // 空配列のみだと中身に対しての型推論が働かないので型アノテーションが必要
+    private tryCount = 0
+
+    setting() {
+        const answerLength = 3
+        while (this.answer.length < answerLength) {
+            const randNum = Math.floor(Math.random() * this.answer.length)
+            const selectedItem = this.answerSource[randNum]
+            if(!this.answer.includes(selectedItem)) {
+                this.answer.push(selectedItem)
+            }
+        }
+    }
+
+    async play() {
+        const inputArr = (await promptInput('「,」区切りで3つの数字を入力してください')).split(',')
+        const result = this.check(inputArr)
+
+        if (result.hit !== this.answer.length) {
+            //不正解だったら続ける
+            printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`)
+            this.tryCount += 1
+            await this.play()
+        } else {
+            // 正解だったら終了
+            this.tryCount += 1
+        }
+    }
+
+    private check(input: string[]) {
+        let hitCount = 0
+        let blowCount = 0
+
+        input.forEach((val, index) => {
+            if (val === this.answer[index]) {
+                hitCount += 1
+            } else if (this.answer.includes(val)) {
+                blowCount += 1
+            }
+        })
+
+        return {
+            hit: hitCount,
+            blow: blowCount,
+        }
+    }
 }
 
 const printLine = (text: string, breakLine: boolean = true) => {
@@ -17,12 +61,9 @@ const promptInput = async (text: string) => {
 }
 
 ;(async () => {
-    const name = await promptInput('名前を入力してください')
-    console.log(name)
-    const age = await promptInput('年齢を入力してください')
-    console.log(age)
-    process.exit()
     const hitAndBlow = new HitAndBlow()
+    hitAndBlow.setting()
+    await hitAndBlow.play()
 })()
 
 
